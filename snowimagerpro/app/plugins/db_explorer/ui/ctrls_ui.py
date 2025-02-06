82236787d84b28c41f4145f0012f08eba850c28c
lbmnky <lars.h.mewes@gmail.com>
2025-02-06 20:37:44 +0100

    FEATURE: Add help functionality with "What's this?" tooltips and update UI resources

--- snowimagerpro/app/plugins/db_explorer/ui/ctrls_ui.py
+++ snowimagerpro/app/plugins/db_explorer/ui/ctrls_ui.py
@@ -18,6 +18,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QCheckBox, QGridLayout, QHBoxLayout,
     QLabel, QPushButton, QSizePolicy, QSpacerItem,
     QVBoxLayout, QWidget)
+import resources_rc
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -153,17 +154,53 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+#if QT_CONFIG(whatsthis)
+        self.title.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p>DB Explorer</p><p><span style=\" font-size:10pt; font-weight:400;\">The &quot;db_explorer&quot; plugin handles the database of raw images and all connections between raw images. </span></p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.title.setText(QCoreApplication.translate("Form", u"db_explorer ", None))
+#if QT_CONFIG(whatsthis)
+        self.btn_write_to_new_db.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p>Save current database under new filename.</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.btn_write_to_new_db.setText(QCoreApplication.translate("Form", u"Save a copy ...", None))
+#if QT_CONFIG(whatsthis)
+        self.checkBox_autosave.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p>Use this to autosave database after every change.</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.checkBox_autosave.setText(QCoreApplication.translate("Form", u"Autosave", None))
+#if QT_CONFIG(whatsthis)
+        self.btn_remove_image_from_db.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p><span style=\" font-weight:600;\">Remove images</span></p><p><br/></p><p>Removes the highlighted image entries from the database.</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.btn_remove_image_from_db.setText(QCoreApplication.translate("Form", u"remove image(s) from db", None))
+#if QT_CONFIG(whatsthis)
+        self.btn_write_to_curr_db.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p>Save the current database. Creates one backup file (&lt;db_name&gt;.csv.bak) in the same folder.</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.btn_write_to_curr_db.setText(QCoreApplication.translate("Form", u"Save database", None))
+#if QT_CONFIG(whatsthis)
+        self.btn_delete_db.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p>Delete current database (file will end up in Recycle Bin)</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.btn_delete_db.setText(QCoreApplication.translate("Form", u"DELETE database", None))
+#if QT_CONFIG(tooltip)
+        self.btn_add_image_to_db.setToolTip(QCoreApplication.translate("Form", u"<html><head/><body><p>bla</p><p><br/></p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(whatsthis)
+        self.btn_add_image_to_db.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p><span style=\" font-weight:600;\">Add image to database</span></p><p>Select one or several image folders (containing 4 SnowImager images) to add them to the database. Selection should be done in the order: &quot;ref&quot;, &quot;ngr&quot; and &quot;gri&quot;. If done in this order, most database parameters &quot;ref group&quot;, dark group&quot;, etc. will be population automatically and correctly.</p><p><img src=\":/doc/resources/doc/add_images_to_db_filedialog.png\"/></p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.btn_add_image_to_db.setText(QCoreApplication.translate("Form", u"Add image(s) to db", None))
+#if QT_CONFIG(whatsthis)
+        self.btn_add_db.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p>Add or create a .csv database</p><p><br/></p><p>Find a database and add it, or enter a new file name to create a new database.</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.btn_add_db.setText(QCoreApplication.translate("Form", u"Add/create database", None))
+#if QT_CONFIG(whatsthis)
+        self.pb_paste_metadata.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p>Paste selected metadata to highlighted image entries</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.pb_paste_metadata.setText(QCoreApplication.translate("Form", u"Paste metadata", None))
         self.pb_autolink_images.setText(QCoreApplication.translate("Form", u"Auto-link images (experimental)", None))
+#if QT_CONFIG(whatsthis)
+        self.pb_copy_metadata.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p><span style=\" font-weight:600;\">Copy metadata</span></p><p><br/></p><p>Select and copy metadata from current image</p><p><br/></p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.pb_copy_metadata.setText(QCoreApplication.translate("Form", u"Copy metadata ...", None))
+#if QT_CONFIG(whatsthis)
+        self.btn_remove_db.setWhatsThis(QCoreApplication.translate("Form", u"<html><head/><body><p>Remove database from SnowImagerPro (file will remain on disk)</p></body></html>", None))
+#endif // QT_CONFIG(whatsthis)
         self.btn_remove_db.setText(QCoreApplication.translate("Form", u"remove database", None))
     # retranslateUi
 
